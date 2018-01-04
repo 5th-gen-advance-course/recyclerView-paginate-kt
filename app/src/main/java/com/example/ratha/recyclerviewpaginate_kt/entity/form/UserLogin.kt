6df556e18740cf43439ac0eb23cc0df64d6cfe6c
rathana-voy <rathanavoy07@gmail.com>
2018-01-04 11:50:18 +0700

    add sub package to entity

---
+++ app/src/main/java/com/example/ratha/recyclerviewpaginate_kt/entity/form/UserLogin.kt
@@ -0,0 +1,9 @@
+package com.example.ratha.recyclerviewpaginate_kt.entity.form
+
+/**
+ * Created by ratha on 1/4/2018.
+ */
+
+data class UserLogin (var name: String ,var email : String , var password: String){
+
+}
